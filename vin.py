cd7d1788749c3c721314cb4819e104f5fe637ccb
lowee1 <63391242+lowee1@users.noreply.github.com>
2024-05-23 15:59:15 +0800

    Add PDM, argparse and main file structure

--- vin.py
+++ vin.py
@@ -1,3 +1,37 @@
-from lexer import Lexer
-from parser import Parser
-from interpreter import Interpreter
+import argparse
+from vparser import Parser
+from vinterpreter import Interpreter
+from vlexer import Lexer
+
+parser = argparse.ArgumentParser(prog="vin", description="Vin programming language")
+parser.add_argument("file", type=str, help="Vin source code file", required=False)
+
+args = parser.parse_args()
+
+if args.file:
+    with open(args.file, "r") as file:
+        text = file.read()
+
+    lexer = Lexer(text)
+    lexer.parse_tokens()
+
+    parser = Parser(lexer.tokens)
+    parser.parse()
+
+    interpreter = Interpreter(parser.ast)
+    interpreter.interpret()
+else:
+    while True:
+        try:
+            text = input("vin> ")
+        except EOFError:
+            break
+
+        lexer = Lexer(text)
+        lexer.parse_tokens()
+
+        parser = Parser(lexer.tokens)
+        parser.parse()
+
+        interpreter = Interpreter(parser.ast)
+        interpreter.interpret()
