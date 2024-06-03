8ba6726c273aa3c5c0e583193e7c61b629a13a7d
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2024-06-03 14:51:10 +0800

    tokens and keywords

--- clexer.py
+++ clexer.py
@@ -1,6 +1,49 @@
-TOKENS = {
-    
-}
+class TOKENS:
+    LeftParen = "LeftParen"
+    RightParen = "RightParen"
+    LeftBrace = "LeftBrace"
+    RightBrace = "RightBrace"
+    LeftBracket = "LeftBracket"
+    RightBracket = "RightBracket"
+    Period = "Period"
+    Comma = "Comma"
+    Colon = "Colon"
+    Keyword = "Keyword"
+    Identifier = "Identifier"
+    String = "String"
+    Number = "Number"
+    Or = "Or"
+    Not = "Not"
+    And = "And"
+    Equiv = "Equiv"
+    NotEquiv = "NotEquiv"
+    Gt = "Gt"
+    Gte = "Gte"
+    Lt = "Lt"
+    Lte = "Lte"
+    Plus = "Plus"
+    Minus = "Minus"
+    Asterisk = "Asterisk"
+    Slash = "Slash"
+    EOF = "EOF"
+
+
+KEYWORDS = [
+    "Agency",
+    "suspect",  # variables
+    "possibly",
+    "employs",  # imports
+    "agent",
+    "expects",
+    "report",  # functions
+    "stakeout",
+    "surveillance",
+    "until",  # loops
+    "investigate",
+    "then",
+    "fallback",  # conditionals
+]
+
 
 class Lexer:
     def __init__(self, text):
@@ -9,4 +52,3 @@ class Lexer:
         self.current = 0
         self.line = 1
         self.column = 0
-
